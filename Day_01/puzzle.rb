673cb8ab7e2a39aafc80c10633767672399bd6f8
Vickybeebzz <sr.conejo@icloud.com>
2022-12-03 19:24:46 +0100

    Add Day1

---
+++ Day_01/puzzle.rb
@@ -0,0 +1,23 @@
+elves = [];
+file_data = File.read("input.txt");
+elves = file_data.split(/\n{2,}/);
+total_calories = [];
+data = [];
+elf_calories = 0;
+max_calories = 0;
+
+elves.each do |counter|
+data = counter.split(" ");
+data.each do |calories|
+elf_calories = elf_calories + calories.to_i;
+end
+total_calories.push(elf_calories);
+elf_calories = 0;
+end
+
+total_calories.each do |total|
+  if total > max_calories
+  max_calories = total;
+  end
+end
+puts max_calories;
