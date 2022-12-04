4c2e967f06065dd27ffd5362cd1b91e7dcc05388
Vickybeebzz <sr.conejo@icloud.com>
2022-12-04 03:07:01 +0100

    Add 2nd part of day01

--- Day_01/puzzle2.rb
+++ Day_01/puzzle2.rb
@@ -1,43 +1,46 @@
-file_data = File.read("input2.txt");
-rounds = [];
-score = 0;
+file_data = File.read("input1.txt");
+elves = [];
 
-rounds = file_data.split(/\n/);
+elves = file_data.split(/\n{2,}/);
+total_calories = [];
 
-rounds.each do |round|
-  round_moves = round.split;
+data = [];
+elf_calories = 0;
+max_calories = 0;
+second_max_calories = 0;
+third_max_calories = 0;
 
-  case round_moves[0]
-  when 'A'
-    case round_moves[1]
-    when 'X'
-      score = score + 1 + 3
-    when 'Y'
-      score = score + 2 + 6
-    when 'Z'
-      score = score + 3 + 0
-    end
+elves.each do |counter|
+  data = counter.split(" ");
 
-  when 'B'
-    case round_moves[1]
-    when 'X'
-      score = score + 1 + 0
-    when 'Y'
-      score = score + 2 + 3
-    when 'Z'
-      score = score + 3 + 6
-    end
+  data.each do |calories|
+    elf_calories = elf_calories + calories.to_i;
+  end
 
-  when 'C'
-    case round_moves[1]
-    when 'X'
-      score = score + 1 + 6
-    when 'Y'
-      score = score + 2 + 0
-    when 'Z'
-      score = score + 3 + 3
-    end
+  total_calories.push(elf_calories);
+  elf_calories = 0;
+end
+
+total_calories.each do |total|
+  if total > max_calories
+    max_calories = total;
   end
 end
 
-puts score;
+total_calories.delete(max_calories);
+
+total_calories.each do |total|
+  if total > second_max_calories
+    second_max_calories = total;
+  end
+end
+
+total_calories.delete(second_max_calories);
+
+total_calories.each do |total|
+  if total > third_max_calories
+    third_max_calories = total;
+  end
+end
+
+puts max_calories+second_max_calories+third_max_calories;
