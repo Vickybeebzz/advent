01754aa45d9fdda882025dab7b766cfd6c3cfb9e
Vickybeebzz <sr.conejo@icloud.com>
2022-12-06 02:24:07 +0100

    Add day05

---
+++ Day_05/puzzle2.rb
@@ -0,0 +1,77 @@
+
+file_data = File.read("input2.txt");
+input = file_data.split(/\n{2,}/);
+crane = [];
+
+
+def read_map (map)
+  counter = 0;
+  map_array = [];
+
+  map.each do |row|
+    characters= row.grapheme_clusters;
+
+    groups = characters.each_slice(4).to_a;
+
+    groups.each do |group|
+      if (counter < 8)
+      group.pop
+      counter=counter+1;
+      end
+    end
+
+    map_array.push(groups);
+  end
+
+  map_array.pop;
+  map_array.map { |item| item.map{ |n| n.join("")}}
+end
+
+def fill_piles(map)
+  box_piles = [[],[],[],[],[],[],[],[],[]];
+  for j in 0..8 do
+    for i in 0..7 do
+      if(map[i][j][0]!= " ")
+        box_piles[j].push(map[i][j]);
+      end
+
+    end
+  end
+  box_piles;
+end
+
+def parse_instructions(instructions)
+  sentences = instructions.split(/\n/);
+  sentences = sentences.map{|item| item.split(" ")}
+
+  sentences.each do |item|
+    item.delete("move")
+    item.delete("from")
+    item.delete("to")
+  end;
+end
+
+map = input[0].split(/\n/);
+map = read_map(map);
+
+instructions = parse_instructions(input[1]);
+
+box_piles = fill_piles(map);
+
+box_piles = box_piles.map {|pile| pile.reverse()}
+
+instructions.each do |instruction|
+  for i in 0..instruction[0].to_i-1 do
+    crane.push(box_piles[(instruction[1].to_i-1)][-1]);
+    box_piles[(instruction[1].to_i-1)].pop
+  end
+  
+  for i in 0..crane.length-1 do
+    box_piles[(instruction[2].to_i-1)].push(crane[-1]);
+    crane.pop;
+  end
+end
+
+box_piles.each do |pile|
+  printf pile[-1];
+end
